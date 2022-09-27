9385a923a042c146d6683eaf930fb95b9d008570
QuarterHeaven <liaotx2@gmail.com>
2022-09-28 01:46:25 +0800

    rewrite README.org. Disabled auto save.

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -1,3 +1,7 @@
+(setq make-backup-files nil)
+(setq-default auto-save-timeout 15) ; 15秒无动作,自动保存
+(setq-default auto-save-interval 100) ; 100个字符间隔, 自动保存
+
 (setq url-gateway-method 'socks)
 (setq socks-server '("Default server" "127.0.0.1" 1081 5))
 
